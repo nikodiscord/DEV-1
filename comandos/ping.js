60b990a3ab7f5c1d5835431a4c0387b88b3c7c3e
Niko/Dev <46076276+nikodiscord@users.noreply.github.com>
2018-12-23 18:28:28 -0200

    Update and rename comandos/ping.js to  comandos/ping.js

--- comandos/ping.js
+++ comandos/ping.js
@@ -1,10 +1,10 @@
-const Discord = require("discord.js");
+constconst Discord = require("discord.js");
 
 exports.run = (bot,message,args) => {
     let embed = new Discord.RichEmbed()
 
-    .setTitle("Meu ping invocador!")
-    .setColor("#09b0e9")
+    .setTitle("Meu ping!")
+    .setColor("#00c4eb")
     .setDescription(`**Meu ping é de** ${Math.round(bot.ping)}ms!`)
     .setFooter(`Comando de ping`, bot.user.displayAvatarURL);
 
@@ -14,3 +14,6 @@ exports.run = (bot,message,args) => {
 exports.help = {
 	name: "ping"
 }
+
+//AVISO PARA ABRIR A CMD NA PASTA , APERTE E SEGURE SHIFT + APERTA O LADO DIRETO DO MOUSE
+//IRÁ APARECER A OPÇÃO ABRIR CMD OU ABRIR JANELA DE COMANDO AQUI
